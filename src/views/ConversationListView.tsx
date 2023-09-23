a5250aa44cb1cddcc6e39fc0a8890f0ef4db3236
haranrk <haranrajkumar97@gmail.com>
2023-09-23 18:16:32 -0400

    add navbar

--- src/views/ConversationListView.tsx
+++ src/views/ConversationListView.tsx
@@ -23,23 +23,23 @@ export default function ConversationListView(): ReactElement {
 
   return (
     <div>
-      <button
+      {/* <button
         onClick={() => setReadReceiptsEnabled(!readReceiptsEnabled)}
         className="bg-blue-100 p-1 my-2 text-xs"
         id={`read-receipt-${readReceiptsEnabled}`}
       >
         {readReceiptsEnabled ? "Disable read receipts" : "Enable read receipts"}
-      </button>
+      </button> */}
       {conversations?.length == 0 && <p>No conversations yet.</p>}
       {conversations
         ? conversations.map((conversation, i) => (
-            <Link to={`c/${conversation.topic}`} key={conversation.topic}>
-              <ConversationCellView
-                conversation={conversation}
-                latestMessage={latestMessages[i]}
-              />
-            </Link>
-          ))
+          <Link to={`c/${conversation.topic}`} key={conversation.topic}>
+            <ConversationCellView
+              conversation={conversation}
+              latestMessage={latestMessages[i]}
+            />
+          </Link>
+        ))
         : "Could not load conversations"}
     </div>
   );
--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -4,6 +4,7 @@ import { useClient, useSetClient } from "../hooks/useClient";
 import { shortAddress } from "../util/shortAddress";
 import { Link } from "react-router-dom";
 import Header from "../components/Header";
+import Navbarx from "../components/Navbarx";
 import { useDisconnect } from "wagmi";
 import { Avatar, Spinner, Card } from '@ensdomains/thorin'
 import { Profile } from '@ensdomains/thorin'
@@ -15,58 +16,10 @@ export default function HomeView(): ReactElement {
   const ensName = useEnsName({ address: client.address });
   const ensAvatar = useEnsAvatar({ address: client.address });
 
-  const copyToClipBoard = async (text: string) => {
-    try {
-      await navigator.clipboard.writeText(text)
-    } catch (err) {
-      console.error('Failed to copy text: ', err)
-    }
-  }
-  function copy() {
-    navigator.clipboard.writeText(client.address);
-    setCopied(true);
-    setTimeout(() => {
-      setCopied(false);
-    }, 2000);
-  }
-
-  const { disconnectAsync } = useDisconnect();
-  const setClient = useSetClient();
-  async function logout() {
-    await disconnectAsync();
-    indexedDB.deleteDatabase("DB");
-    localStorage.removeItem("_insecurePrivateKey");
-    setClient(null);
-  }
 
   return (
     <div className="p-4 pt-14">
-      <Header>
-        <div className="flex justify-between">
-          <div>
-            <div style={{ maxWidth: '50px' }}>
-              <Profile
-                address={client.address}
-                avatar={ensAvatar?.data || undefined}
-                ensName={ensName?.data || undefined}
-                dropdownItems={[
-                  {
-                    label: 'Copy Address',
-                    color: 'text',
-                    onClick: () => copyToClipBoard(client.address),
-                  }
-                ]}
-              />
-            </div>
-            {/* <button className="text-xs text-zinc-600" onClick={copy}>
-              {copied ? "Copied Address!" : "Copy Address"}
-            </button> */}
-          </div>
-          <div>
-            <button onClick={logout}>Logout</button>
-          </div>
-        </div>
-      </Header>
+      <Navbarx></Navbarx>
       <div className="mt-10">
         <small className="flex justify-between">
           <span>Here are your conversations:</span>
