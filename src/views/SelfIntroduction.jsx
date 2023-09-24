275401cddb2acf0a33aee3479390f95ab47a8922
Vikranth Kanumuru <20688709+highskillzz@users.noreply.github.com>
2023-09-23 19:56:32 -0700

    Self intro, without backend done

--- src/views/SelfIntroduction.jsx
+++ src/views/SelfIntroduction.jsx
@@ -9,77 +9,101 @@ const categories = [
   "Web Design",
 ];
 
-const ExperienceLevel = () => {
-  return (
-    <Card>
-      <Select
-        autocomplete
-        options={[
-          { value: "0", label: "Beginner" },
-          { value: "1", label: "Intermediate" },
-          { value: "2", label: "Advanced" },
-        ]}
-        placeholder="Select an option..."
-        tabIndex="2"
-      />
-    </Card>
-  );
-};
+// const ExperienceLevel = () => {
+//   return (
+// <Card>
+// <Select
+//   options={[
+//     { value: "0", label: "Beginner" },
+//     { value: "1", label: "Intermediate" },
+//     { value: "2", label: "Advanced" },
+//   ]}
+//   placeholder="Choose Experience..."
+//   size="small"
+// />
+// </Card>
+//   );
+// };
 
 const SelfIntroduction = () => {
   return (
-    <div className="p-10">
-      <h1 className="text-4xl mb-4 text-center">Tell us about Yourself</h1>
-      <h2 className="text-2xl mb-4 text-left">
-        How would you categorize yourself?
-      </h2>
+    <div className="p-10 flex flex-col">
+      <h1 className="text-4xl mb-8 text-center">Tell us about Yourself</h1>
+      <div className="mx-auto">
+        <h2 className="text-2xl mb-4 text-left">
+          How would you categorize yourself?
+        </h2>
 
-      <div className="flex flex-wrap justify-start items-start rounded-lg bg-gray-100 pl-4 mb-4">
-        {[
-          "Graphic Designer",
-          "Illustrator",
-          "Animator",
-          "UI/UX",
-          "Web Design",
-        ].map((title, index) => (
-          <div
-            key={index}
-            className="flex flex-col items-center m-2 p-2 w-32 h-64 rounded-lg border border-gray-300"
-          >
-            <img
-              src={`path/to/${title}.jpg`}
-              alt={title}
-              className="w-24 h-24 rounded-full mb-2"
-            />
-            <span className="text-center">{title}</span>
-            <div>
-              {" "}
-              {/* ExperienceLevel Component */}
-              {/* Your Experience Level Component Goes Here */}
+        <div className="flex flex-wrap justify-between p-10 items-start rounded-lg border border-gray-300 mb-4">
+          {[
+            {
+              title: "Graphic Designer",
+              image: "./graphic-design.png",
+            },
+            {
+              title: "Illustrator",
+              image: "./flag-garland.svg",
+            },
+            {
+              title: "Animator",
+              image: "./flash-circle-1.svg",
+            },
+            {
+              title: "UI/UX",
+              image: "./interface-ui-layout-app.svg",
+            },
+            {
+              title: "Web Design",
+              image: "./page.svg",
+            },
+          ].map((object, index) => (
+            <Card
+              key={index}
+              className="flex flex-col w-30 h-34 items-center ml-5 rounded-lg"
+            >
+              <img
+                src={`./${object.image}`}
+                alt={object.title}
+                className="w-24 h-24 rounded-full mb-2"
+              />
+              <span className="text-center">{object.title}</span>
+              <div>
+                {" "}
+                {/* ExperienceLevel Component */}
+                {/* Your Experience Level Component Goes Here */}
+                {/* <ExperienceLevel /> */}
+              </div>
+            </Card>
+          ))}
+        </div>
+
+        <div className="mb-4">
+          <label htmlFor="interest" className="block text-lg mb-2">
+            What are you interested in learning more about?
+          </label>
+
+          <textarea
+            id="interest"
+            className="w-full p-2 h-32 border rounded-lg"
+          ></textarea>
+        </div>
+
+        <div className="mb-4">
+          <label htmlFor="bio" className="block text-lg mb-2">
+            Please provide a brief bio for people to know more about you:
+          </label>
+          <textarea
+            id="bio"
+            className="w-full p-2 h-32 border rounded-lg"
+          ></textarea>
+        </div>
+        <div class=" mt-2  rounded-lg mx-auto justify-center items-center text-center">
+          <div class=" h-[40px] pl-11 pr-[43px] py-[23px] bg-gradient-to-b from-pink-300 via-purple-400 to-lime-800 rounded-lg justify-center items-center inline-flex">
+            <div class="w-[140px] text-center text-white text-base font-semibold font-['Inter'] leading-tight">
+              Next Step
             </div>
           </div>
-        ))}
-      </div>
-
-      <div className="mb-4">
-        <label htmlFor="interest" className="block text-lg mb-2">
-          What are you interested in learning more about?
-        </label>
-        <input
-          id="interest"
-          type="text"
-          className="w-full p-2 border rounded-lg"
-        />
-      </div>
-
-      <div className="mb-4">
-        <label htmlFor="bio" className="block text-lg mb-2">
-          Please provide a brief bio for people to know more about you:
-        </label>
-        <textarea
-          id="bio"
-          className="w-full p-2 h-32 border rounded-lg"
-        ></textarea>
+        </div>
       </div>
     </div>
   );
