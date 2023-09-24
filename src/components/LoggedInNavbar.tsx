91ebbde53ae356bc6a039b9ab24455ec88d4c83d
haranrk <haranrajkumar97@gmail.com>
2023-09-23 23:30:11 -0400

    fix nav

--- src/components/LoggedInNavbar.tsx
+++ src/components/LoggedInNavbar.tsx
@@ -19,9 +19,6 @@ export default function LoggedInNavbar({
         </Link>
         <div className="flex md:order-2">
           {client ? <LoggedInProfile></LoggedInProfile> : <LoggedOutProfile></LoggedOutProfile>}
-          {/* <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get started</button> */}
-          <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
-          </button>
         </div>
         <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
           <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
