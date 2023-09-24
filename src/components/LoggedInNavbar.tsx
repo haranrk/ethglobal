d6a7471681e3f9e94bad9031bfc168d84a434955
haranrk <haranrajkumar97@gmail.com>
2023-09-23 23:29:14 -0400

    fix url

--- src/components/LoggedInNavbar.tsx
+++ src/components/LoggedInNavbar.tsx
@@ -13,10 +13,10 @@ export default function LoggedInNavbar({
 
     <nav className="fixed w-full z-20 top-0 left-0 border-b backdrop-blur-md bg-opacity-30 bg-white shadow-sm dark:border-gray-600">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="https://flowbite.com/" className="flex items-center">
+        <Link to="/" className="flex items-center">
           <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo" />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">MakerMeet</span>
-        </a>
+        </Link>
         <div className="flex md:order-2">
           {client ? <LoggedInProfile></LoggedInProfile> : <LoggedOutProfile></LoggedOutProfile>}
           {/* <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get started</button> */}
