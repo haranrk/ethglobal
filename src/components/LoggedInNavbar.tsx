48231b3675912d8ada3ec82ce179117bece23531
Vikranth Kanumuru <20688709+highskillzz@users.noreply.github.com>
2023-09-23 22:36:24 -0700

    Commit checkboxes are not  showing

--- src/components/LoggedInNavbar.tsx
+++ src/components/LoggedInNavbar.tsx
@@ -7,20 +7,31 @@ import { Link } from "react-router-dom";
 export default function LoggedInNavbar({
   children,
 }: PropsWithChildren<unknown>): ReactElement {
-
-  const client = useClient()!;
+  const client = useClient();
   return (
-
     <nav className="fixed w-full z-20 top-0 left-0 border-b backdrop-blur-md bg-opacity-30 bg-white shadow-sm dark:border-gray-600">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <Link to="/" className="flex items-center">
-          <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo" />
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">MakerMeet</span>
+          <img
+            src="https://flowbite.com/docs/images/logo.svg"
+            className="h-8 mr-3"
+            alt="Flowbite Logo"
+          />
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+            MakerMeet
+          </span>
         </Link>
         <div className="flex md:order-2">
-          {client ? <LoggedInProfile></LoggedInProfile> : <LoggedOutProfile></LoggedOutProfile>}
+          {client ? (
+            <LoggedInProfile></LoggedInProfile>
+          ) : (
+            <LoggedOutProfile></LoggedOutProfile>
+          )}
         </div>
-        <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+        <div
+          className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+          id="navbar-sticky"
+        >
           <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
               <Link className="text-gray-900" to="new">
@@ -38,10 +49,20 @@ export default function LoggedInNavbar({
               </Link>
             </li>
             <li>
-              <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
+              <a
+                href="#"
+                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              >
+                Services
+              </a>
             </li>
             <li>
-              <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+              <a
+                href="#"
+                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              >
+                Contact
+              </a>
             </li>
           </ul>
         </div>
@@ -72,5 +93,5 @@ export default function LoggedInNavbar({
     //     </div>
     //   </div>
     // </Header>
-  )
+  );
 }
